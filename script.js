8bc898aaa839961aca6415095916c57c95ff1db4
Mikel Farrell <farrellmikel@yahoo.com>
2024-03-17 21:16:11 +0000

    Add 16x16 grid using flexbox.

---
+++ script.js
@@ -0,0 +1,10 @@
+const grid = document.querySelector(".grid");
+
+// Use a nested loop to create the grid cells
+for (let i = 0; i < 16; i++) { // loop 16 times for rows
+  for (let j = 0; j < 16; j++) { // loop 16 times for columns
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    grid.appendChild(cell);
+  }
+};
