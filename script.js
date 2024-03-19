0a7d147b6c724ec77debf62cdcc67f90dc3be0a7
Mikel Farrell <farrellmikel@yahoo.com>
2024-03-19 21:59:56 +0000

    Add Random colour, Eraser, and Clear grid button options.

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-function etchSketch(userChoice) {
-  // Target the main grid container and style it.
-  const grid = document.querySelector(".container");
-  grid.style.width = (userChoice * 32) + "px";
-  grid.style.height = (userChoice * 32) + "px";
-  grid.style.margin = "20px";
+// Target the main grid container and style it.
+const grid = document.querySelector(".container");
+grid.style.width = "592px";
+grid.style.height = "592px";
+grid.style.margin = "20px";
+grid.style.border = "15px solid hsl(0, 0%, 67%)"
+grid.style.borderRadius = "10px";
 
+function etchSketch(userChoice) {
   // Use a nested loop to create the grid cells.
-  for (let i = 0; i < userChoice; i++) { // loop 16 times for rows
-    for (let j = 0; j < userChoice; j++) { // loop 16 times for columns
+  for (let i = 0; i < userChoice; i++) { // for loop for rows
+    for (let j = 0; j < userChoice; j++) { // for loop for columns
       const cell = document.createElement("div");
       cell.classList.add("cell");
       grid.appendChild(cell);
-      cell.style.width = "30px";
-      cell.style.height = "30px";
-      cell.style.border = "1px solid black";
+      cell.style.width = (592 / userChoice - 2) + "px";
+      cell.style.height = (592 / userChoice - 2) + "px";
+      cell.style.border = "1px solid hsl(0, 0%, 67%)";
       cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "gray";  
+      cell.style.backgroundColor = "hsl(217, 100%, 61%)";  
       });
     }
   };
 };
 
-// Target the reset button and style it.
-const btn = document.querySelector("#btn");
-btn.textContent = "Create sketch";
-btn.addEventListener("click", () => {
+// function resetSketch() {
+//   const container = document.querySelector(".container");
+//   if (container) {
+//     container.remove();
+//   }
+// };
+
+// Target the sketch button and style it.
+const sketchBtn = document.querySelector("#sketchBtn");
+sketchBtn.textContent = "Create sketch";
+sketchBtn.addEventListener("click", () => {
   let userChoice = prompt("Select the number of squares per side:");
-  if (userChoice > 20) {
+  if (userChoice > 99 || userChoice === "") {
     return;
   } else {
     etchSketch(userChoice);
   }
 });
+
+const randomColor = document.querySelector("#randomColor");
+randomColor.textContent = "Random color";
+const eraser = document.querySelector("#eraser");
+eraser.textContent = "Eraser";
+const clearGrid = document.querySelector("#clearGrid");
+clearGrid.textContent = "Clear grid"
